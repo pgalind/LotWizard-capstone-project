8730892e00564eda4eee424869f70ecb142762cc
Pablo Galindo <pgalind@users.noreply.github.com>
2022-10-07 17:14:48 -0400

    up to date forms, cleaned extra files

--- f22-team23-driver-incentive-app/hooks/signIn.js
+++ f22-team23-driver-incentive-app/hooks/signIn.js
@@ -1,35 +1,45 @@
-import axios from "axios"
-import user from '../services/user'
-//import { useRouter } from 'next/router';
+import axios from 'axios';
+import user from '../services/user';
+import { useRouter } from 'next/router';
 
-export default function signIn(values){
-    //const router = useRouter(); // to redirect
+export default function signIn(values) {
+  const router = useRouter(); // to redirect
 
-    axios.post('/api/authenticateUser',{
-        userName : values.username
-    }).then((response) => {
-        let passwordResponse = response.data
-
-        console.log("user password is : " + passwordResponse)
-        console.log("user name is : " + values.username)
-
-        if(passwordResponse === 0){
-            alert('Username or Password is incorrect')
-        } else {
-            if(values.password == passwordResponse){
-                alert("Successful log in: Hello " + values.username + "!")
-                user.name = values.username
-                console.log(user.name)
-                //console.log("HELLO ${userName}!")
-                //router.push('../');
-            } else {
-                alert ('Username or Password is incorrect')
-            }
-        }
-
-     
-
-    }).catch((error) => {
-        console.log("Does exist error : " + error)
+  axios
+    .post('/api/authenticateUser', {
+      userName: values.username,
     })
+    .then((response) => {
+      let passwordResponse = response.data;
+
+      console.log('user password is : ' + passwordResponse);
+      console.log('user name is : ' + values.username);
+
+      if (passwordResponse === 0) {
+        alert('Username or Password is incorrect');
+      } else {
+        if (values.password == passwordResponse) {
+          //alert("Successful log in: Hello " + values.username + "!")
+          user.name = values.username;
+          console.log(user.name);
+          //console.log("HELLO ${userName}!")
+          //router.push('../');
+          router.push(
+            {
+              pathname: '/',
+              query: { session: true },
+            },
+            '/'
+          );
+        } else {
+          alert('Username or Password is incorrect');
+        }
+      }
+    })
+    .catch((error) => {
+      console.log('Does exist error : ' + error);
+    })
+    .finally(() => {
+      setSubmitting(false);
+    });
 }
