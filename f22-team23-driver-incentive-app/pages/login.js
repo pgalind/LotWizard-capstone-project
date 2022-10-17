424deb283027a7c5af368563ec8c697fbee69cf7
Pablo Galindo <pgalind@users.noreply.github.com>
2022-10-17 09:44:39 -0400

    fixed forms but version is outdated

--- f22-team23-driver-incentive-app/pages/login.js
+++ f22-team23-driver-incentive-app/pages/login.js
@@ -17,73 +17,73 @@ const Login = () => {
 
   return (
     <div>
-      <span>
+      <span className="p-4">
         <Link href="../">Exit SignIn</Link>
       </span>
-    <div className="flex items-center justify-center">
-      {success === 'true' && (
-        <div className="pt-10 pb-10 color-green-300">You're signed up!</div>
-      )};
-      <Formik
-        initialValues={{
-          username: '',
-          password: '',
-        }}
-        validationSchema={loginSchema}
-        onSubmit={signIn}
-        validateOnMount={false}
-        validateOnChange={false}
-        validateOnBlur={false}
-      >
-        {({
-          isSubmitting,
-          errors,
-          values,
-          handleChange,
-          handleBlur,
-          handleSubmit,
-        }) => (
-          <form className="max-w-full p-10" onSubmit={handleSubmit}>
-            <h1 className="font-bold text-2xl mb-6 text-center">Sign in</h1>
-            <FormSection>
-              <FormInput
-                label="Username"
-                type="text"
-                name="username"
-                onChange={handleChange}
-                onBlur={handleBlur}
-                value={values?.username}
-                isError="true"
-                error={errors?.username}
-              />
-            </FormSection>
-
-            <FormSection>
-              <FormInput
-                label="Password"
-                type="password"
-                name="password"
-                onChange={handleChange}
-                onBlur={handleBlur}
-                value={values?.password}
-                isError="true"
-                error={errors?.password}
-              />
-            </FormSection>
-
-            <FormSection>
-              <FormLink href="/register">
-                "Don't have an account? Register.
-              </FormLink>
-            </FormSection>
-
-            <SubmitButton isSubmitting={isSubmitting} />
-          </form>
+      <div className="flex items-center justify-center">
+        {success === 'true' && (
+          <div className="pt-10 pb-10 color-green-300">You're signed up!</div>
         )}
+        <Formik
+          initialValues={{
+            username: '',
+            password: '',
+          }}
+          validationSchema={loginSchema}
+          onSubmit={signIn}
+          validateOnMount={false}
+          validateOnChange={false}
+          validateOnBlur={false}
+        >
+          {({
+            isSubmitting,
+            errors,
+            values,
+            handleChange,
+            handleBlur,
+            handleSubmit,
+          }) => (
+            <form className="max-w-full p-10" onSubmit={handleSubmit}>
+              <h1 className="font-bold text-2xl mb-6 text-center">Sign in</h1>
+              <FormSection>
+                <FormInput
+                  label="Username"
+                  type="text"
+                  name="username"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values?.username}
+                  isError="true"
+                  error={errors?.username}
+                />
+              </FormSection>
+
+              <FormSection>
+                <FormInput
+                  label="Password"
+                  type="password"
+                  name="password"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values?.password}
+                  isError="true"
+                  error={errors?.password}
+                />
+              </FormSection>
+
+              <FormSection>
+                <FormLink href="/register">
+                  "Don't have an account? Register.
+                </FormLink>
+              </FormSection>
+
+              <SubmitButton isSubmitting={isSubmitting} />
+            </form>
+          )}
         </Formik>
-        </div>
       </div>
+    </div>
   );
-}
+};
 
 export default Login;
