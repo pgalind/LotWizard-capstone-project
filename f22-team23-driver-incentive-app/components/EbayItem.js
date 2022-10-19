c2c5f774537b51344b0ecbbc8d9a2c797c467ec2
Pablo Galindo <pgalind@users.noreply.github.com>
2022-10-18 20:24:38 -0400

    cahnges to catalog UI

--- f22-team23-driver-incentive-app/components/EbayItem.js
+++ f22-team23-driver-incentive-app/components/EbayItem.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Link from 'next/link';
-import token from '../lib/token';
+//import { token } from '../lib/token';
 
 // This variable inconsistently increments when it's declared inside EbayItem() for some reason
 var imageIndex = 0;
@@ -9,7 +9,7 @@ var imageIndex = 0;
 // NOTE: Ebay's auth token changes often; If the page is stuck on loading
 // Please generate a new auth token by going to https://developer.ebay.com/my/api_test_tool?index=0
 // and set it to auth value in axios call
-export default function EbayItem() {
+export default function EbayItem(itemID) {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState('');
   const [post, setPost] = useState({});
@@ -42,11 +42,12 @@ export default function EbayItem() {
   //{loading ? 'Loading' : post.title}
   //{error ? error : null}
   var auth =
-    'Bearer v^1.1#i^1#r^0#p^1#I^3#f^0#t^H4sIAAAAAAAAAOVYD2wTVRhf908bGAtM0UwM9QD/wd296/Wu7UErhTJXZWtHxxgzMK5378qx9q7ce926iGzMxAgoCQIhRLPMhEQTFPwDBhIjGhKNMRiCEhI1MZAQp4JCSPw3pt61ZXSTALImLrG5pHnf+973fr/f+7733h3orbQ/+lz9c79W2e4oHegFvaU2GzMJ2Csr5k4pK62tKAEFDraB3tm95X1lgwuQmEykhGUQpXQNQUcmmdCQkDX6iLShCbqIVCRoYhIiAUtCNNCwVHBSQEgZOtYlPUE4QkEf4QSyIrs5SQKcG/CcZFq1qzGbdR+hxFxe4GQVr6i4ZRnyZj9CaRjSEBY1bI13OkkGkAzTDBiB4QXAUryXbyMcLdBAqq6ZLhQg/Fm4QnasUYD1xlBFhKCBzSCEPxSoi4YDoeCSxuYFdEEsf16HKBZxGo1uLdZl6GgRE2l442lQ1luIpiUJIkTQ/twMo4MKgatgbgN+TuqYOyZ6PbwHOt0cxypFkbJON5IivjEOy6LKpJJ1FaCGVdx9M0VNNWLroITzrUYzRCjosP6a0mJCVVRo+IgliwIrA5EI4V8id4mG3BgnkahA2VA7yciyIMl5YqKZV4xMSm7ODVmJy0+Ui5aXecxMi3VNVi3RkKNRx4ugiRqO1QYUaGM6hbWwEVCwhajQj72qoQe0WYuaW8U0XqtZ6wqTphCObPPmKzAyGmNDjaUxHIkwtiMrkY8QUylVJsZ2ZnMxnz4Z5CPWYpwSaLqrq4vqYindiNNOABi6tWFpVFoLkyJh+lq1nvNXbz6AVLNUJGiORKqAu1MmloyZqyYALU74XTznZD153UfD8o+1/sNQwJkeXRHFqhA5xvIul5dj3Rwri25nMSrEn09S2sIBY2I3mRSNDohTCVGCpGTmWToJDVUWWE4xtVEgKfNehXR5FYWMcTJPMgqEAMJYTPJ6/k+FcqupHoWSAXFRcr1oed5Wl1FBOObpbJIbl4aXRxu8QX6l1sg6E+sioQ73+kWdwfWtT7gCT9bFfbdaDdclvzihmso0m/MXQwCr1osnQr2OMJTHRS8q6SkY0ROq1D2xFpg15Iho4O4oTCRMw7hIBlKpUHH26qLR+5fbxO3xLt4Z9R+dT9dlhayUnVisrPHIDCCmVMo6gShJT9JWreuief2wzO1Z1OPirZo31wnF2iSZY6vKuSsnlaVLoU6JMiDS04Z526bC1g2sWe+AmnmeYUNPJKDRwoy7npPJNBZjCTjRCrsICa6KE+ywZXiec3HAy7Pj4iVlj9L2ibYlFWMrLn/8Nq/V9OiXfH9J9sf02Q6BPtvbpTYboMEcZhZ4oLJseXnZ5FqkYkipokIhNa6Z764GpDpgd0pUjdJK28YGoelUwWeFgVXg3pEPC/YyZlLBVwYw41pPBVN9T5XTyQDGenjAtoFZ13rLmenld/V8d+a3O3+Eg/bV29+4sqnp4Nm7m34HVSNONltFSXmfraRy0w9HVnfJv/Sc3jB/5uCWaP179LBj/wcrOi+EdjieP1I9r2fq11sztX0nX3u2ehW/077smde3ZE5lTnxZ79v/8v2Tg+KL0oUFJw8fOyOFp8+dPrkHz9t+5fBjC//8qyb+9DenQw/te2H75taqmds2RGoGptkvf1622/nmukd+pl85f+nogTXnPur/cOjg9wMtvXvenzPcP7Sr+vhX9RXvnO3vGTy2d0amwt5KoqG3AlPO12h2+tXl7e0dJ7/d/e7CE4d2bLuvaejTl1av6K/dsHHwj33DsxvoqXumfnL8yFM/MVOO4q3xEwRYUf3gtEmhj9XKhr3Va84euHhl52eXdzWfu/hFT8qxeXfN8KWj8x/OLd/fkT15tfARAAA=';
+    'Bearer v^1.1#i^1#r^0#f^0#I^3#p^1#t^H4sIAAAAAAAAAOVYbWwURRju9a6VApVI+FAEOa7wg4/bm9372lt6lxwthdP2rnClgQbEvdnZY9u93WVnru3FxFwbKBiJhBhNxC/Er2CUSJQE0OAPQjSIIFG0xkQ+Cko0/tEgKgru3h3lWgkgvcQm3p/LvPPOO8/zzPvOzA7IVlbN61vad6naclf5jizIllss9DhQVVkx/25r+bSKMlDkYNmRnZ219Vov1GI+JWvccoQ1VcHI3p2SFczljEFHWlc4lccS5hQ+hTBHIBcPNzVyDAU4TVeJClXZYY/UBx1QTIiJBBtgGCgwKMEYVuVazBY16OD9iA4wbjEQcHsYGnmMfozTKKJgwisk6GAAwzhp4KTZFprl3B7O7ad8wNfmsLciHUuqYrhQwBHKweVyY/UirDeHymOMdGIEcYQi4YZ4LBypXxxtqXUVxQoVdIgTnqTx0FadKiB7Ky+n0c2nwTlvLp6GEGHscIXyMwwNyoWvgbkD+DmphYTfh3gAIQR+wPj5kkjZoOopntwch2mRBKeYc+WQQiSSuZWihhqJdgRJoRU1QkTq7ebfsjQvS6KE9KBj8aLwqnBzsyO0WOjidSGadGJeRIIudTqbl9c7vWyCh8BLC07o9/qRG3oLE+WjFWQeNlOdqgiSKRq2R1WyCBmo0XBtmCJtDKeYEtPDIjERFfkxoKChN8C2mYuaX8U0WaeY64pShhD2XPPWKzA4mhBdSqQJGowwvCMnkVE2miYJjuGduVwspE83DjrWEaJxLldXVxfV5aZUPeliAKBdK5sa43AdShkZ0p0yaz3vL916gFPKUYHIGIkljmQ0A0u3kasGACXpCHl8XsbNFnQfCis03PoPQxFn19CKKFWFeAIMpFkPj0Qf6xchXYoKCRWS1GXiQAk+40zxegcimsxD5IRGnqVTSJcEzu0VDW1E5BR8AdHpCYiiM+EVfE5aRAgglEjAAPt/KpTbTfU4gjoiJcn1kuV5W0O3BGIJtnOZEG2MrYg3Bep9q5Som5HbmyMd/vWLOuvXr3zQE36oIRm83Wq4Ifk6WTKUaTHmL4UAZq2XToSlKiZIGBG9OFQ11KzKEsyMrgV260Izr5NMHMmyYRgRybCmRUqzV5eM3r/cJu6Md+nOqP/ofLohK2ym7OhiZY7HRgBekyjzBKKgmnKZta7yxvXDNK/NoR4Rb8m4uY4q1gbJPFtJyF85qRxdCndCSkdYTevGbZuKmTewFrUDKcZ5RnRVlpHeSo+4nlOpNOETMhpthV2CBJf4UXbY0j6fj6ZZ4PePiBfMHaVrR9uWVIqt2LbkDq/VrqEf+aGy3I/utewFvZY95RYLcIE5dA2YVWldYbOOn4YlgiiJFyksJRXj21VHVAfKaLykl1daHmvilp0selbYsQbcO/iwUGWlxxW9MoDp13sq6AlTqxmGBjRLs26P298Gaq732ugptkk9hxfgR479dF9PQ9bKbryyoPZi33ugetDJYqkos/Vaysr2xvah+z8eeGr7s9zWrq9PHN0wZmd2arX1beFY/+TXpkzedkI7+/Tjx7cH+7f1ZU+dP97/666Nq+5ufGdi1dYzD8MXhbrZnX/Me3PPBWX/1CuZ9sAHz3Mnn/nxiTHf9s9t6Hlu4QH7hOqr4i+fn257I3PA89WC3/4aCHz0VtnMscH3yZHYanAu2QguzHx008GKOfqeUHjbA3BG9OquizsP7j69ofaV8WsmdXy6M/bhn9LluTXzbU2xb7amZ6xYPX3zUS3z8pkvf/7u0KVPDsS/X9h6ru78qwd/Xz9r4uWmgXbQs3QLd/aedydt2b2kxrJx3Ben5C0/tAv79h+J7UcvtB7a9NlAcvOsKvLS69vHHn4yv3x/A9PV5XPwEQAA';
+
   useEffect(() => {
     axios({
       method: 'get',
-      url: 'https://api.ebay.com/buy/browse/v1/item/v1|374289166032|0',
+      url: `https://api.ebay.com/buy/browse/v1/item/v1|${itemID}|0`,
       headers: {
         Authorization: auth,
         'Content-Type': 'application/json',
@@ -55,14 +56,16 @@ export default function EbayItem() {
           'contextualLocation=country=<2_character_country_code>,zip=<zip_code>,affiliateCampaignId=<ePNCampaignId>,affiliateReferenceId=<referenceId></referenceId>',
       },
     })
-      .then((response) => {
+      .then((res) => {
+        res.json();
+        console.log(res);
+      })
+      .then((data) => {
         setLoading(false);
-        setPost(response.data);
-        setError('');
-        console.log(response);
-        console.log(response.data.title);
-        console.log(response.data.price.value);
-        setImage(response.data.image.imageUrl);
+        setPost(data);
+        setImage(data.image.imageUrl);
+        console.log(data.title);
+        console.log(data.price.value);
       })
       .catch((error) => {
         setLoading(false);
@@ -70,16 +73,17 @@ export default function EbayItem() {
         setError("Couldn't retrive catalog info from ebay :/");
         console.log(error);
       });
-  }, []);
+  }, [itemID]);
+
   return Object.keys(post).length ? (
-    <div className="card">
-      <Link href={'/catalog/5'}>
+    <div className="flex space-between align-center">
+      <Link href={`/catalog/${itemID}`}>
         <a>
           <img src={image} alt={image} className="w-96 h-96" />
         </a>
       </Link>
       <div className="flex flex-col items-center justify-center p-5">
-        <Link href={'/catalog/5'}>
+        <Link href={`/catalog/${itemID}`}>
           <a>
             <h2 className="text-lg">{post.title}</h2>
           </a>
