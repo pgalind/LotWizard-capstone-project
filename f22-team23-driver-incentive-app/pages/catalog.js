738828d112039255e449a967f935a7f43d32a180
Semicedevine <semicedevine@icloud.com>
2022-10-20 13:20:00 -0400

    store oauth tokens in cookies to reduce minting

--- f22-team23-driver-incentive-app/pages/catalog.js
+++ f22-team23-driver-incentive-app/pages/catalog.js
@@ -4,15 +4,42 @@ import Layout from '../components/Layout';
 import { useSession } from 'next-auth/react';
 import ItemsLink from '../components/ItemsLink';
 import EbayItem from '../components/EbayItem';
+import axios from 'axios';
+import Cookie from 'js-cookie';
 
 export default function Catalog() {
   //const { data: session } = useSession();
-  return (
-    <Layout title="Home Page">
-      <h1 className="text-lg">Sponsor A's Catalog</h1>
-      <h2>includes the following...</h2>
-      <h3></h3>
-      <EbayItem></EbayItem>
-    </Layout>
-  );
+  const [token, setToken] = useState('');
+  useEffect(() => {
+    if (typeof Cookie.get('token') == 'undefined') {
+      axios.post('/api/token', {}).then((response) => {
+        setToken('Bearer ' + response.data.access_token);
+        Cookie.set('token', 'Bearer ' + response.data.access_token);
+        console.log('Got new token:');
+        console.log('Bearer ' + response.data.access_token);
+      });
+    } else {
+      console.log('used old token');
+      setToken(Cookie.get('token'));
+    }
+  }, []);
+
+  if (token == '') {
+    return (
+      <Layout title="Home Page">
+        <h1 className="text-lg">Sponsor A's Catalog</h1>
+        <h2>includes the following...</h2>
+        <h3></h3>
+      </Layout>
+    );
+  } else {
+    return (
+      <Layout title="Home Page">
+        <h1 className="text-lg">Sponsor A's Catalog</h1>
+        <h2>includes the following...</h2>
+        <h3></h3>
+        <EbayItem token={token} index={0} />
+      </Layout>
+    );
+  }
 }
