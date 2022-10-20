44c464e541f320074025fb0bb2d6d58ba1531eb8
Semicedevine <semicedevine@icloud.com>
2022-10-20 13:15:04 -0400

    add query database for sponsor items api endpoint

--- f22-team23-driver-incentive-app/pages/api/querySponsorItems.js
+++ f22-team23-driver-incentive-app/pages/api/querySponsorItems.js
@@ -1,22 +1,21 @@
-import excuteQuery from '../../lib/db'
+import excuteQuery from '../../lib/db';
 
 export default async (req, res) => {
+  try {
+    console.log('about to run the queryItems query');
+    console.log(req.body);
 
-    try {
-        console.log("about to run the queryItems query")
-        console.log(req.body)
+    //interpolate the variables into the query
+    let queryString = `SELECT * FROM SponsorA`;
+    console.log('Full query string : ' + queryString);
 
-        //interpolate the variables into the query
-        let queryString = `SELECT * FROM SponsorA`
-        console.log("Full query string : " + queryString)
-         
-       let result = await excuteQuery({
-           query: queryString,
-           values: [req.body],
-       });
-      
-       res.send(result)
-  } catch ( error ) {
-      console.log( error );
+    let result = await excuteQuery({
+      query: queryString,
+      values: [req.body],
+    });
+
+    res.send(result);
+  } catch (error) {
+    console.log(error);
   }
-  };
+};
