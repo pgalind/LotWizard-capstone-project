112c462d219d0e274c15961134718377ba7a04fc
Kaleb Knecht <kknecht@clemson.edu>
2022-11-06 12:02:59 -0500

    Basic buggy version of application decisions

--- f22-team23-driver-incentive-app/pages/viewApplications.js
+++ f22-team23-driver-incentive-app/pages/viewApplications.js
@@ -1,17 +1,20 @@
 // This page will be viewed by a driver when they are choosing a sponsor to apply to
 
 import React, { useState, useEffect } from 'react';
-import Link from 'next/link';
 import user from '../services/user';
 import axios from 'axios';
 import { useFormik } from 'formik';
 import ExitButton from '../components/ExitButton';
-//import getPoints from '../hooks/getUserPoints';
+import 'reactjs-popup/dist/index.css';
+import FormSection from '../components/FormSection';
+import Select from 'react-select';
+import SubmitButton from '../components/SubmitButton';
 
 export default function userProfile() {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState('');
   const [applications, setApplications] = useState();
+  const [isSubmitting, setSubmitting] = useState(false);
 
   useEffect(() => {
     let data = { sponsorUserName: user.name };
@@ -30,21 +33,32 @@ export default function userProfile() {
       });
   }, []);
 
-  /*const formik = useFormik({
-        initialValues: {
-          sponsorToApply: '',
-          reason: '',
-        },
-        onSubmit: values => {
-          //alert(JSON.stringify(values, null, 2)); // TODO: axios post to send application to sponsor
-          console.log("Reason: " + values.reason)
-          console.log("Sponsor: " + values.sponsorToApply)
-          let data = { driver: user.name,
-                        reason: values.reason,
-                        sponsor: values.sponsorToApply };
-          axios.post('/api/sendDriverApplication', data).then
-        },
+  // Options for sponsor to accept or reject individual applications
+  const options = [
+    { value: 'accepted', label: 'Accept' },
+    { value: 'rejected', label: 'Reject' }
+  ]
+
+  const formik = useFormik({
+    initialValues: {
+      decision: 'Undecided',
+    },
+    onSubmit: (values, actions) => {
+      console.log('Decision: ' + values.decision);
+      let data = {
+        sponsor: user.name,
+        decision: values.decision
+      };
+      /*axios.post('/api/sendDriverApplication', data).then((res) => {
+        actions.setSubmitting(false);
       });*/
+    },
+  });
+
+  function submitDecision(driverID, decision) {
+    console.log(driverID + ": " + decision)
+  }
+
 
   if (loading) {
     return <div>Loading . . .</div>;
@@ -53,24 +67,40 @@ export default function userProfile() {
     <div className="p-10">
       <ExitButton />
 
-      <h1 className="font-bold text-xl mb-6">Apply to a Sponsor</h1>
+      <h1 className="font-bold text-xl mb-6">Open Applications</h1>
+
+      <form
+        className="flex flex-col items-center w-[300px] min-w-full"
+        onSubmit={formik.handleSubmit}
+      ></form>
 
       <table>
         <tr>
           <th>Driver</th>
           <th>Reason</th>
-          <th>Link</th>
+          <th>Decision</th>
+          <th>Submit</th>
         </tr>
         {applications.map((val, key) => {
           return (
             <tr key={key}>
               <td>{val['DriverID']}</td>
               <td>{val['Application_Reason']}</td>
-              <td><a href="./decideApplication" className="p-2 hover:text-blue-600">Click to Decide</a></td>
+              <td>
+              <FormSection name={key}>
+                <Select 
+                    options={options} 
+                    onChange={value=>formik.setFieldValue('decision',value.label)}
+                />
+               </FormSection>
+                </td>
+              <td><button className="p-2 hover:text-blue-600" onClick={() => submitDecision(val['DriverID'], formik.values.decision)}>Submit</button></td>
             </tr>
           );
         })}
       </table>
+
+
     </div>
   );
 }
